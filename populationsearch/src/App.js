bb92aa01d8c97a46ca93071813c2f21180bce1d7
Ajay Banstola <ajay.banstola@gmail.com>
2019-08-03 05:27:02 +0545

    made a country drop down

--- populationsearch/src/App.js
+++ populationsearch/src/App.js
@@ -2,21 +2,15 @@ import React from "react";
 import Select from "react-select";
 import PostData from "../src/dataService.json";
 
-const options = [
-  { value: "blues", label: "Blues" },
-  { value: "rock", label: "Rock" },
-  { value: "jazz", label: "Jazz" },
-  { value: "orchestra", label: "Orchestra" }
-];
-
 class App extends React.Component {
   render() {
+    console.log(PostData);
     return (
       <div>
-        <Select options={options} />
-        <Select options={options} />
+        <Select options={PostData} />
+        <Select options={PostData} />
         {PostData.map((postDetail, index) => {
-          return <h1>{postDetail.name}</h1>;
+          return <h1 key={index}>{postDetail.name}</h1>;
         })}
       </div>
     );
